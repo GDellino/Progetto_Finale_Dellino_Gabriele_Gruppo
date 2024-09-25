e34424124603cf0c5b04ec3a84fcc0c024e8a33e
Gabriele Dellino <gdellino37@gamil.com>
2024-09-25 15:41:52 +0200

    User Story 8 Done

--- app/Livewire/CreateArticleForm.php
+++ app/Livewire/CreateArticleForm.php
@@ -9,6 +9,7 @@ use Livewire\WithFileUploads;
 use Livewire\Attributes\Validate;
 use App\Jobs\GoogleVisionLabelImage;
 use App\Jobs\GoogleVisionSafeSearch;
+use App\Jobs\RemoveFaces;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 
@@ -25,65 +26,71 @@ class CreateArticleForm extends Component
     #[Validate('required')]
     public $category;
     public $article;
-    public $images=[];
+    public $images = [];
     public $temporary_images;
 
 
-    public function updatedTemporaryImages(){
-        if($this->validate([
-            'temporary_images.*'=>'image|max:1024',
-            'temporary_images' =>'max:6'
-        ]))
-        {
-            foreach($this->temporary_images as $image){
-                $this->images[]=$image;
+    public function updatedTemporaryImages()
+    {
+        if ($this->validate([
+            'temporary_images.*' => 'image|max:1024',
+            'temporary_images' => 'max:6'
+        ])) {
+            foreach ($this->temporary_images as $image) {
+                $this->images[] = $image;
             }
         }
     }
-    public function removeImage($key){
-        if(in_array($key,array_keys($this->images))){
+    public function removeImage($key)
+    {
+        if (in_array($key, array_keys($this->images))) {
             unset($this->images[$key]);
         }
     }
-    public function save(){
+    public function save()
+    {
         $this->validate();
-        $this->article=Article::create([
+        $this->article = Article::create([
             'title' => $this->title,
             'description' => $this->description,
             'price' => $this->price,
             'category_id' => $this->category,
             'user_id' => Auth::id()
-            ]);
+        ]);
 
-            if(count($this->images)>0){
-                foreach($this->images as $image){
-                    $newFileName = "articles/{$this->article->id}";
-                    $newImage = $this->article->images()->create(['path'=>$image->store('images','public')]);
-                    dispatch(new ResizeImage($newImage->path, 300, 300));
-                    dispatch(new GoogleVisionSafeSearch($newImage->id));
-                    dispatch(new GoogleVisionLabelImage($newImage->id));
+        if (count($this->images) > 0) {
+            foreach ($this->images as $image) {
+                $newFileName = "articles/{$this->article->id}";
+                $newImage = $this->article->images()->create(['path' => $image->store('images', 'public')]);
+                // dispatch(new ResizeImage($newImage->path, 300, 300));
+                // dispatch(new GoogleVisionSafeSearch($newImage->id));
+                // dispatch(new GoogleVisionLabelImage($newImage->id));
 
-                }
-                File::deleteDirectory(storage_path('/app/livewire-tmp'));
+                RemoveFaces::withChain([
+                    new ResizeImage($newImage->path, 300, 300),
+                    new GoogleVisionSafeSearch($newImage->id),
+                    new GoogleVisionLabelImage($newImage->id),
+                ])->dispatch($newImage->id);
             }
+            File::deleteDirectory(storage_path('/app/livewire-tmp'));
+        }
 
 
 
-            session()->flash('success', 'Articolo creato con successo');
-            $this->cleanForm();
+        session()->flash('success', 'Articolo creato con successo');
+        $this->cleanForm();
     }
 
-    protected function cleanForm(){
-        $this->title='';
-        $this->description='';
-        $this->category='';
-        $this->price='';
-        $this->images=[];
+    protected function cleanForm()
+    {
+        $this->title = '';
+        $this->description = '';
+        $this->category = '';
+        $this->price = '';
+        $this->images = [];
     }
     public function render()
     {
         return view('livewire.create-article-form');
     }
-
-
 }
