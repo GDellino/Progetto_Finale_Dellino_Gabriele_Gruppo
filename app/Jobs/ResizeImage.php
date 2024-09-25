8cdf9826f2efd339580db58312bb6bc648c06dcc
Gabriele Dellino <gdellino37@gamil.com>
2024-09-25 15:51:10 +0200

    User Story 9 Done

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -3,6 +3,7 @@
 namespace App\Jobs;
 
 use Spatie\Image\Image;
+use Spatie\Image\Enums\Unit;
 use Spatie\Image\Enums\CropPosition;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
@@ -36,6 +37,14 @@ class ResizeImage implements ShouldQueue
 
         Image::load($scrPath)
         ->crop($w, $h, CropPosition::Center)
+        ->watermark(
+            base_path('public/images/logofinale1.png'),
+            width:50,
+            height:50,
+            paddingX:5,
+            paddingY:5,
+            paddingUnit:Unit::Percent,
+        )
         ->save($destPath);
     }
 }
