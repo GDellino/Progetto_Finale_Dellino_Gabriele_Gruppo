84a7d5fbd52de9f2b6dc855fc14c7349f7feb059
Davide Pizarra Roxas <davidepizarra@gmail.com>
2024-10-01 12:59:20 +0200

    article show modifiche

--- resources/views/article/searched.blade.php
+++ resources/views/article/searched.blade.php
@@ -2,14 +2,14 @@
     <div class="container-fluid vh-100 container-custom mb-3  hNav">
         <div class="row">
             <div class="col-12">
-                <h1 class="display-3 text-center mt-3">
+                <h1 class="display-3 text-center mt-5">
                     {{__('ui.searchResults')}} <span class="fst-italic">{{ $query }}</span>
                 </h1>
             </div>
         </div>
-        <div class='row justify-content-center  align-items-center'>
+        <div class='row justify-content-center  align-items-center mt-5'>
             @forelse ($articles as $article)
-                <div class="col-12 col-md-3 ">
+                <div class="col-12 col-md-3 mt-3 ">
                     <x-card :article="$article" />
                 </div>
             @empty
